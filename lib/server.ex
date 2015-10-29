ed324f0f39bef325de0380593c6779344a5bd628
Josh Petitt <joshpetitt@jpmec.com>
2015-10-28 21:48:07 -0500

    Added logging to server

--- lib/server.ex
+++ lib/server.ex
@@ -1,11 +1,17 @@
+require Logger
+
 defmodule ExTimeWinService.Server do
   def start(port) do
+    Logger.info('ExTimeWinService.Server starting')
+
     tcp_options = [:binary, {:packet, 0}, {:active, false}]
     {:ok, socket} = :gen_tcp.listen(port, tcp_options)
     listen(socket)
   end
 
   defp listen(socket) do
+    Logger.info('ExTimeWinService.Server listening')
+
     {:ok, conn} = :gen_tcp.accept(socket)
     spawn(fn -> recv(conn) end)
     listen(socket)
@@ -14,9 +20,11 @@ defmodule ExTimeWinService.Server do
   defp recv(conn) do
     case :gen_tcp.recv(conn, 0) do
       {:ok, data} ->
+        Logger.info('ExTimeWinService.Server connection received data')
         :gen_tcp.send(conn, data)
         recv(conn)
       {:error, :closed} ->
+        Logger.info('ExTimeWinService.Server connection closed')
         :ok
     end
   end
